6b8d0d950a33389a677fd66c032cc7dee49a81a9
Erik Huang <erikhuangdrums@gmail.com>
2021-03-09 05:35:30 -0800

    gets current position for geolocation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        pos => console.log(pos),
+        err => console.log(err)
+    );
     return (
         <div>
             Hello there.
