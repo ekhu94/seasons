40247882735cc3c5a6cc2e172381a72b98f89269
Erik Huang <erikhuangdrums@gmail.com>
2021-03-09 10:07:12 -0800

    add changes to state

--- src/index.js
+++ src/index.js
@@ -7,15 +7,27 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {lat: null};
+        this.state = { lat: null, long: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            pos => {
+                this.setState({
+                    long: pos.coords.longitude,
+                    lat: pos.coords.latitude
+                })
+            },
+            err => console.log(err)
+        );
     }
 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            pos => console.log(pos),
-            err => console.log(err)
+        return (
+            <h2>
+                Latitude: { this.state.lat } <br />
+                Longitude: { this.state.long }
+            </h2>
+            // <h2>Longitude: { this.state.long }</h2>
         );
-        return <div>Latitude: </div>
     }
 }
 
