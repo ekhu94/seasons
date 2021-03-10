41a7dc40c36e62682266e14db4de0827b3a6b51b
Erik Huang <erikhuangdrums@gmail.com>
2021-03-09 16:32:35 -0800

    add appropriate text based on season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
 const SeasonDisplay = props => {
-    return (
-        <div>{ props.lat }</div>
-    );
+    const season = getSeason(props.lat, new Date().getMonth());
+    const textDisplay = season === 'summer' ? 'Winter is coming.' : 'Winter is here.';
+
+    return <h1>{ textDisplay }</h1>
 };
 
 export default SeasonDisplay;
