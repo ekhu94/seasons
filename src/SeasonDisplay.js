d9c7736162a522498274579d02620697d52b8e8a
Erik Huang <erikhuangdrums@gmail.com>
2021-03-09 16:43:25 -0800

    add semantic icons to SeasonDisplay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,8 +11,15 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
     const textDisplay = season === 'summer' ? 'Winter is coming.' : 'Winter is here.';
+    const icon = season === 'summer' ? 'sun' : 'snowflake';
 
-    return <h1>{ textDisplay }</h1>
+    return (
+        <div>
+            <i className={ `${icon} icon` } />
+            <h1>{ textDisplay }</h1>
+            <i className={ `${icon} icon` } />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
